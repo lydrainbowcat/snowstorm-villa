be0600e2f4bbed096fe640e2aff5209d9e7337ef
ITX351 <itx351@126.com>
2018-06-28 23:19:18 +0800

    移动失败时消耗移动次数；更新role.js敏锐、善战、推理属性

--- src/lib/constants/role.js
+++ src/lib/constants/role.js
@@ -3,115 +3,172 @@ const ROLES = [
     name: "guide",
     title: "导游",
     methods: ["blade", "bludgeon"],
-    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "hat", "hanging", "earring"]
+    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "hat", "hanging", "earring"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "propsman",
     title: "道具师",
     methods: ["blade", "bludgeon", "strangle", "shoot", "trap", "poison"],
-    clews: ["man_foot", "man_hand", "woman_foot", "woman_hand", "soil", "smell", "snack", "smartphone", "phone", "cloth", "glove", "glassed", "gun", "hat", "hanging", "earring", "watch"]
+    clews: ["man_foot", "man_hand", "woman_foot", "woman_hand", "soil", "smell", "snack", "smartphone", "phone", "cloth", "glove", "glassed", "gun", "hat", "hanging", "earring", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "forensic_doctor",
     title: "法医",
     methods: ["blade", "bludgeon", "strangle", "poison"],
-    clews: ["man_foot", "smell", "smartphone", "glove", "glassed", "hat", "watch"]
+    clews: ["man_foot", "smell", "smartphone", "glove", "glassed", "hat", "watch"],
+    keen: 1,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "high_school_student",
     title: "高中生",
     methods: ["blade", "bludgeon"],
-    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "glassed", "hanging", "earring"]
+    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "glassed", "hanging", "earring"],
+    keen: 1,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "manager",
     title: "管理员",
     methods: ["blade", "bludgeon", "strangle", "trap", "poison"],
-    clews: ["man_foot", "soil", "snack", "smartphone", "glove", "hat", "watch"]
+    clews: ["man_foot", "soil", "snack", "smartphone", "glove", "hat", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "coach",
     title: "教练",
     methods: ["blade", "bludgeon", "strangle"],
-    clews: ["man_foot", "man_hand", "phone", "cloth", "glassed", "hanging", "watch"]
+    clews: ["man_foot", "man_hand", "phone", "cloth", "glassed", "hanging", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "police_woman",
     title: "警察",
     methods: ["blade", "bludgeon", "strangle", "shoot"],
-    clews: ["woman_foot", "phone", "cloth", "glove", "gun", "hat", "watch"]
+    clews: ["woman_foot", "phone", "cloth", "glove", "gun", "hat", "watch"],
+    keen: 1,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "soldier",
     title: "军人",
     methods: ["blade", "bludgeon", "strangle", "shoot"],
-    clews: ["man_foot", "man_hand", "phone", "cloth", "gun", "hat", "watch"]
+    clews: ["man_foot", "man_hand", "phone", "cloth", "gun", "hat", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 1
   },
   {
     name: "hunter",
     title: "猎人",
     methods: ["blade", "shoot", "trap"],
-    clews: ["woman_foot", "woman_hand", "soil", "smell", "phone", "cloth", "gun"]
+    clews: ["woman_foot", "woman_hand", "soil", "smell", "phone", "cloth", "gun"],
+    keen: 1,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "conjurator",
     title: "灵媒",
     methods: ["blade", "bludgeon"],
-    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "cloth", "hanging", "earring"]
+    clews: ["woman_foot", "woman_hand", "snack", "smartphone", "cloth", "hanging", "earring"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "male_tourist",
     title: "男驴友",
     methods: ["blade", "strangle", "trap"],
-    clews: ["man_foot", "man_hand", "soil", "smartphone", "cloth", "hat", "watch"]
+    clews: ["man_foot", "man_hand", "soil", "smartphone", "cloth", "hat", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "male_doctor",
     title: "男医生",
     methods: ["blade", "bludgeon", "strangle", "poison"],
-    clews: ["man_foot", "man_hand", "snack", "phone", "glassed", "hat", "watch"]
+    clews: ["man_foot", "man_hand", "snack", "phone", "glassed", "hat", "watch"],
+    keen: 1,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "female_tourist",
     title: "女驴友",
     methods: ["blade", "bludgeon", "trap"],
-    clews: ["woman_foot", "woman_hand", "soil", "smartphone", "cloth", "hat", "earring"]
+    clews: ["woman_foot", "woman_hand", "soil", "smartphone", "cloth", "hat", "earring"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "female_doctor",
     title: "女医生",
     methods: ["blade", "bludgeon", "poison"],
-    clews: ["woman_foot", "smell", "snack", "smartphone", "glove", "hanging", "watch"]
+    clews: ["woman_foot", "smell", "snack", "smartphone", "glove", "hanging", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "misterious_man",
     title: "神秘人",
     methods: ["blade", "bludgeon", "strangle", "shoot", "trap", "poison"],
-    clews: ["man_foot", "smartphone", "cloth", "glove", "glassed", "gun", "hat"]
+    clews: ["man_foot", "smartphone", "cloth", "glove", "glassed", "gun", "hat"],
+    keen: 0,
+    inference: 0,
+    fierce: 1
   },
   {
     name: "twins",
     title: "双胞胎",
     methods: ["blade", "bludgeon"],
-    clews: ["woman_foot", "woman_hand", "smartphone", "phone", "cloth", "glassed", "hanging"]
+    clews: ["woman_foot", "woman_hand", "smartphone", "phone", "cloth", "glassed", "hanging"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "student",
     title: "学生",
     methods: ["bludgeon", "trap"],
-    clews: ["man_foot", "man_hand", "smartphone", "glassed", "hat", "hanging", "watch"]
+    clews: ["man_foot", "man_hand", "smartphone", "glassed", "hat", "hanging", "watch"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   },
   {
     name: "detective",
     title: "侦探",
     methods: ["trap", "poison"],
-    clews: ["snack", "phone", "glove", "glassed", "hat", "hanging", "watch"]
+    clews: ["snack", "phone", "glove", "glassed", "hat", "hanging", "watch"],
+    keen: 0,
+    inference: 1,
+    fierce: 0
   },
   {
     name: "writer",
     title: "作家",
     methods: ["blade", "bludgeon"],
-    clews: ["man_foot", "man_hand", "smartphone", "cloth", "glassed", "hat", "hanging"]
+    clews: ["man_foot", "man_hand", "smartphone", "cloth", "glassed", "hat", "hanging"],
+    keen: 0,
+    inference: 0,
+    fierce: 0
   }
 ];
 
