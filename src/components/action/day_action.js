8f3414a11b09a9b9a0f98b0bd0989c79f98274e5
lydrainbowcat <hyqjlyd@qq.com>
2018-07-04 16:45:00 +0800

    加入DayActionStore，DayAction行动按钮UI调整

--- src/components/action/day_action.js
+++ src/components/action/day_action.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { observer } from "mobx-react";
-import Movement from "./movement";
+import { Combobox } from "react-widgets";
 import Tooltip from "../common/tooltip";
 
 import PERIOD from "../../lib/constants/period";
 import gameStore from "../../lib/store/game_store";
+import placeStore from "../../lib/store/place_store";
 import CommonProcessor from "../../lib/processor/common";
+import dayActionStore from "../../lib/store/day_action_store";
 
 @observer
 class DayAction extends React.Component {
@@ -16,9 +18,11 @@ class DayAction extends React.Component {
     this.handleActiveKillerTrack = this.handleActiveKillerTrack.bind(this);
   }
 
-  handleMove(location) {
+  handleMove() {
     const {role} = this.props;
+    const location = dayActionStore.getMovementOfRole(role);
     CommonProcessor.actDayMove(role, location, true);
+    dayActionStore.setMovementOfRole(role, role.location);
   }
 
   handleActiveKillerTrack(e) {
@@ -26,40 +30,59 @@ class DayAction extends React.Component {
     CommonProcessor.activeKillerTrack(this.props.role);
   }
 
+  renderMovement(role) {
+    const period = gameStore.period;
+    const disabled = role.movement < 1;
+    if (period === PERIOD.INITIAL_SELECT || period === PERIOD.DAY_ACT) {
+      return <div className="col-5">
+        <div className="row align-items-center col-thin-gutters">
+          <div className="col-9">
+            <Combobox
+              data={placeStore.places}
+              value={dayActionStore.getMovementOfRole(role)}
+              valueField="name"
+              textField="title"
+              disabled={disabled}
+              onChange={value => dayActionStore.setMovementOfRole(role, value)}
+            />
+          </div>
+          <div className="col-3">
+            {disabled ||
+            <button className="btn btn-outline-primary btn-sm" onClick={this.handleMove}>
+              移动
+            </button>}
+          </div>
+        </div>
+      </div>
+    } else {
+      return "";
+    }
+  }
+
   render() {
     const {role} = this.props;
-    const period = gameStore.period;
-
-    let movementPart = null;
-    if (period === PERIOD.INITIAL_SELECT || period === PERIOD.DAY_ACT)  {
-      movementPart = <Movement
-        originLocation={role.location}
-        disabled={role.movement < 1}
-        onMove={this.handleMove}
-      />;
-    } else {
-      movementPart = "无行动可用";
-    }
 
     return (
       <div className="row align-items-center spacing-10">
         <div className="col-2 text-right">
           <Tooltip text={role.title}/>
         </div>
-        <div className="col-8">
-          <div className="row">
-            <div className="col-10">
-              {movementPart}
-            </div>
-            {role.killerTrackActivatable && (
-              <div className="col-2 text-right">
-                <button className="btn btn-outline-primary" onClick={this.handleActiveKillerTrack}>
-                  发现凶案
-                </button>
-              </div>
-            )}
+        {this.renderMovement(role)}
+        {
+          role.killerTrackActivatable &&
+          <div className="col-2 thin-gutters">
+            <button className="btn btn-outline-danger btn-sm w-100" onClick={this.handleActiveKillerTrack}>
+              发现凶案
+            </button>
           </div>
-        </div>
+        }
+        {
+          <div className="col-2 thin-gutters">
+            <button className="btn btn-outline-primary btn-sm">
+              test
+            </button>
+          </div>
+        }
       </div>
     );
   }
