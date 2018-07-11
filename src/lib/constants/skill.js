dae508f61c272ee29df4f50e361bc3305e124973
lydrainbowcat <hyqjlyd@qq.com>
2018-07-11 22:34:43 +0900

    人物技能名称和类型

---
+++ src/lib/constants/skill.js
@@ -0,0 +1,184 @@
+const SKILLS = [
+  {
+    name: "dexterous",
+    title: "灵巧",
+    type: 0
+  },
+  {
+    name: "perfume",
+    title: "香水",
+    type: 0
+  },
+  {
+    name: "mind_imply_1",
+    title: "心理暗示1",
+    type: 0
+  },
+  {
+    name: "mind_imply_2",
+    title: "心理暗示2",
+    type: 2
+  },
+  {
+    name: "brain_diagnosis",
+    title: "思维诊疗",
+    type: 2
+  },
+  {
+    name: "criminal_invest",
+    title: "刑事侦查",
+    type: 1
+  },
+  {
+    name: "exploration",
+    title: "探险精神",
+    type: 2
+  },
+  {
+    name: "invitation",
+    title: "行动邀请",
+    type: 2
+  },
+  {
+    name: "special_trap",
+    title: "特制陷阱",
+    type: 0
+  },
+  {
+    name: "meticulous",
+    title: "缜密心思",
+    type: 1
+  },
+  {
+    name: "struggle",
+    title: "求生本能",
+    type: 0
+  },
+  {
+    name: "inspire",
+    title: "鞭策",
+    type: 1
+  },
+  {
+    name: "perfect_memory",
+    title: "完美记忆",
+    type: 1
+  },
+  {
+    name: "safe_check",
+    title: "安全检查",
+    type: 1
+  },
+  {
+    name: "protection",
+    title: "保护",
+    type: 0
+  },
+  {
+    name: "suppress",
+    title: "镇压",
+    type: 2
+  },
+  {
+    name: "suspicion",
+    title: "刑侦",
+    type: 1
+  },
+  {
+    name: "guard",
+    title: "警戒",
+    type: 2
+  },
+  {
+    name: "mediumship",
+    title: "通灵",
+    type: 2
+  },
+  {
+    name: "whitsundays",
+    title: "降灵",
+    type: 1
+  },
+  {
+    name: "propsbox",
+    title: "道具箱",
+    type: 0
+  },
+  {
+    name: "show_mechanism",
+    title: "演出准备-机关锁",
+    type: 2
+  },
+  {
+    name: "show_toy_car",
+    title: "演出准备-玩具巡逻车",
+    type: 2
+  },
+  {
+    name: "show_frighten",
+    title: "演出准备-惊吓盒",
+    type: 2
+  },
+  {
+    name: "show_doll",
+    title: "演出准备-人偶",
+    type: 2
+  },
+  {
+    name: "host_advantage_1",
+    title: "主场优势1",
+    type: 0
+  },
+  {
+    name: "host_advantage_2",
+    title: "主场优势2",
+    type: 1
+  },
+  {
+    name: "escape",
+    title: "逃命",
+    type: 0
+  },
+  {
+    name: "nightmare",
+    title: "旧日梦魇",
+    type: 1
+  },
+  {
+    name: "useless",
+    title: "无用学识",
+    type: 0
+  },
+  {
+    name: "detective",
+    title: "平凡侦探",
+    type: 1
+  },
+  {
+    name: "crime_genius_1",
+    title: "犯罪天才1",
+    type: 0
+  },
+  {
+    name: "crime_genius_2",
+    title: "犯罪天才2",
+    type: 1
+  },
+  {
+    name: "lead_advantage",
+    title: "主角光环",
+    type: 0
+  },
+  {
+    name: "expert_1",
+    title: "轻车熟路1",
+    type: 0
+  },
+  {
+    name: "expert_2",
+    title: "轻车熟路2",
+    type: 1
+  }
+];
+
+export default SKILLS;
